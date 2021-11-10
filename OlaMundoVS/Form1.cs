1b06bf355c8c47ae5e8efe53024dbe8b80f77547
FelipeFFS93 <felipe_freiresilva@yahoo.com.br>
2021-11-09 22:31:55 -0300

    Finalização do curso

---
+++ OlaMundoVS/Form1.cs
@@ -0,0 +1,17 @@
+namespace OlaMundoVS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Seja bem vindo ao curso de C# GUI!!!";
+        }
+
+    }
+}
